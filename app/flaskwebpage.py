6152355ce5222cd2c485a31256addb40bcc43364
Ucnt <matt.svensson@gmail.com>
2017-07-03 12:17:48 -0400

    Update flaskwebpage.py

--- app/flaskwebpage.py
+++ app/flaskwebpage.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, request, redirect
-import sys, traceback
+import sys, os, traceback
 app = Flask(__name__)
 
 @app.route("/") 
@@ -13,16 +13,22 @@ def index():
 
 
 def get_exception():
-    error_type, error_message, exc_traceback = sys.exc_info()
+    exc_type, exc_obj, exc_tb = sys.exc_info()
+    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
     return '''
-Error Message: {error_message}
+Python Version: {version}
+File: {file_name}
 Error Type: {error_type}
-Traceback: {traceback}
-    '''.format(
-                error_type=error_type,
-                error_message=error_message,
-                traceback=exc_traceback.format_exc().strip(),
-              ).strip()
+Error Message: {error_message}
+Traceback: 
+{traceback}
+         '''.format(
+                    version =sys.version.split("(")[0],
+                    file_name = fname,
+                    error_type = exc_type.__name__,
+                    error_message = exc_obj,
+                    traceback = traceback.format_exc().replace(", in ",", in \n"),
+                    )
 
 
 def get_client_ip():
